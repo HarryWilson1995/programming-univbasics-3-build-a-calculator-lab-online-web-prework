40d1b6901941ea305774d3b367d1ae933c29f9a3
Harry Wilson <harry.wilson1995a@gmail.com>
2019-11-18 16:36:00 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  sum = num2 + num1
+  p sum
 end
 
 def subtraction(num1, num2)
-
+  minus = num1 - num2
+  p minus
 end
 
 def division(num1, num2)
-
+  divide = num1 / num2
+  p divide
 end
 
 def multiplication(num1, num2)
-
+  multiply = num1 * num2
+  p multiply
 end
 
 def modulo(num1, num2)
-
+  remainder = num1 % num2
+  p remainder
 end
 
 def square_root(num)
-
+  p Math.sqrt(num)
 end
